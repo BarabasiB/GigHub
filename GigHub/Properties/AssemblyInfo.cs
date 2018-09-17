b1e9e06d5f7574a1abc9bf48b09d9efcefb6d86a
bbarabasi <bbarabasi@gmail.com>
2018-09-17 16:31:54 +0300

    Downloaded start project from tutorial

--- GigHub/Properties/AssemblyInfo.cs
+++ GigHub/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GigHub")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("409ebac6-af42-449c-ae59-eb46a58adffc")]
+[assembly: Guid("d4163bde-e0fa-4cdc-b89b-ea2aed08bb40")]
 
 // Version information for an assembly consists of the following four values:
 //
